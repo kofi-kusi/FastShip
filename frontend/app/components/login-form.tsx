3665caaf5d1ce055f944a1b563eb353f57c82f78
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-11-26 11:48:49 +0000

    Send login credentials to server

--- frontend/app/components/login-form.tsx
+++ frontend/app/components/login-form.tsx
@@ -9,18 +9,32 @@ import {
   FieldSeparator,
 } from "~/components/ui/field";
 import { Input } from "~/components/ui/input";
+import { useContext } from "react";
+import { AuthContext } from "~/contexts/AuthContext";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+
+  const { login } = useContext(AuthContext)
+
+  function loginUser(data: FormData) {
+    const email = data.get("email")
+    const password = data.get("password")
+
+    if ( !email || !password ) {
+      return
+    } 
+
+    login(email.toString(), password.toString())
+
+  }
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8" action={(data) => {
-            console.log(data.get("email"))
-            }}>
+          <form className="p-6 md:p-8" action={loginUser}>
             <FieldGroup>
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Welcome back</h1>
@@ -60,7 +74,7 @@ export function LoginForm({
           </form>
           <div className="bg-muted relative hidden md:block">
             <img
-              src="../../../public/rowan-freeman-clYlmCaQbzY-unsplash.jpg"
+              src="/rowan-freeman-clYlmCaQbzY-unsplash.jpg"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
