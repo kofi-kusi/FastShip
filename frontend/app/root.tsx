749e7d11b6e89157c4e1aa58354fdca6672e51f0
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-11-29 16:36:42 +0000

    Add tanstack/react-query

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -11,6 +11,7 @@ import type { Route } from "./+types/root";
 import "./app.css";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Toaster } from "./components/ui/sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -45,7 +46,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <AuthProvider><Outlet /></AuthProvider>;
+  return (
+    <AuthProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <Outlet />
+      </QueryClientProvider>
+    </AuthProvider>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query"
 import { useContext } from "react"
 import { Navigate } from "react-router"
+import { toast } from "sonner"
 import { AppSidebar } from "~/components/app-sidebar"
 import {
   Breadcrumb,
@@ -16,6 +18,7 @@ import {
   SidebarTrigger,
 } from "~/components/ui/sidebar"
 import { AuthContext } from "~/contexts/AuthContext"
+import api from "~/lib/api"
 
 export default function DashboardPage() {
   const { token } = useContext(AuthContext)
@@ -23,6 +26,20 @@ export default function DashboardPage() {
   if (!token) {
     return <Navigate to="/login" />
   }
+
+  const {isLoading, isError, data} = useQuery({
+    queryKey: ["shipments"],
+    queryFn: async () => {
+      const data = await api.seller.getShipments()
+      return data
+    }
+  })
+
+  if (isError) {
+    toast.error("Failed getting shipments")
+  }
+
+
 
   return (
     <SidebarProvider>
@@ -32,30 +49,34 @@ export default function DashboardPage() {
           <div className="flex items-center gap-2 px-3">
             <SidebarTrigger />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+            
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+          {
+              isLoading || !data ? <h1>Loading ...</h1> : (
+                <div className="flex flex-1 flex-col gap-4 p-4">
+                  <div className="grid auto-rows-min gap-4 md:grid-cols-4">
+                    <NumberLabel value={data.data.length} label="Total Shipments" />
+                    <NumberLabel value={100} label="Placed" />
+                    <NumberLabel value={100} label="In transit" />
+                    <NumberLabel value={100} label="Delivered" />
+                  </div>
+                  <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+                </div>
+              )
+            }
         </div>
       </SidebarInset>
     </SidebarProvider>
+  )
+}
+
+function NumberLabel({ value, label} : { value: number, label: string}) {
+  return (
+    <div className="flex flex-col gap-2 rounded-xl border border-gray-200 p-4">
+      <h1 className="text-4xl font-bold">{value}</h1>
+      <p className="text-gray-500">{label}</p>
+    </div>
   )
 }
